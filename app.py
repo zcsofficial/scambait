903ba7d8454132717b619d6f4b9623b1f8631a60
ZCS <contact.zcsco@gmail.com>
2024-12-10 06:50:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ def log():
     return jsonify({"status": "success", "data": logged_data})
 
 if __name__ == '__main__':
-    app.run(debug=True , host='0.0.0.0', port=8000)
+    app.run(debug=True , host='0.0.0.0', port=5000)
